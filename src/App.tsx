0b5c850527f418803e1a728f5482bbbfbe8a04d6
Ronny Roeller <ronny@collaborne.com>
2021-07-03 22:09:58 +0200

    Add Amplify-UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Auth from '@aws-amplify/auth';
+import { CognitoUserInterface } from '@aws-amplify/ui-components';
+import { AmplifyAuthenticator, AmplifyTotpSetup } from '@aws-amplify/ui-react';
+import { useEffect, useState } from 'react';
+
+Auth.configure({
+  region: 'eu-west-1',
+  userPoolId: 'XXX',
+  userPoolWebClientId: 'XXX',
+});
 
 function App() {
+	const [user, setUser] = useState<CognitoUserInterface>();
+	useEffect(() => {
+    const loadCognitoUser = async () => {
+      setUser(await Auth.currentAuthenticatedUser());
+    };
+    void loadCognitoUser();
+	}, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AmplifyAuthenticator>
+      <AmplifyTotpSetup user={user} />
+    </AmplifyAuthenticator>
   );
 }
 
